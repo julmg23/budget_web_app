73a1ea1f989a1377f703267983c6badff18aaf00
Julia <julmg23@gmail.com>
2022-02-07 15:27:05 -0500

    Created transaction class: id, date, amount, type (income or expense)

--- application.py
+++ application.py
@@ -1,8 +1,20 @@
 import os
 from flask import Flask, flash, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
 
 # Configure application
 app = Flask(__name__)
+
+# Configure SQLAlchemy Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///transactions.db'
+db = SQLAlchemy(app)
+
+class Transaction(db.model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    amount = db.Column(db.Smallmoney, nullable=False)
+    type = db.Column(db.Text, nullable=False)
+
 
 # Convert to USD function
 def usd(value):
