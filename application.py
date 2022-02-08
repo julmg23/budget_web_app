d2ae2a78e32096b5bbccae61b99fa33b34e4cd96
Julia <julmg23@gmail.com>
2022-02-07 19:35:48 -0500

    Began working on transactions route and transactions html

--- application.py
+++ application.py
@@ -1,19 +1,11 @@
-import os
+
 from flask import Flask, flash, redirect, render_template, request
-from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+
 
 # Configure application
 app = Flask(__name__)
-
-# Configure SQLAlchemy Database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///transactions.db'
-db = SQLAlchemy(app)
-
-class Transaction(db.model):
-    id = db.Column(db.Integer, primary_key=True)
-    date = db.Column(db.Date, nullable=False)
-    amount = db.Column(db.Smallmoney, nullable=False)
-    type = db.Column(db.Text, nullable=False)
 
 
 # Convert to USD function
@@ -21,10 +13,33 @@ def usd(value):
     """Format value as USD."""
     return f"${value:,.2f}"
 
+
 @app.route("/")
 def index():
     """Show application page"""
     return render_template("index.html")
+
+
+@app.route("/transactions", methods=["GET", "POST"])
+def transactions():
+    """Show transactions page"""
+
+    # Configure sqlite3 Database
+    con = sqlite3.connect('transactions.db')
+    cur = con.cursor()
+
+    # Retrieve all transactions and display on page
+    all_transactions = cur.execute("SELECT date, price, type FROM transactions ORDER BY date")
+
+    if request.method == "POST":
+
+
+        cur.execute("INSERT INTO transactions (date, price, type) VALUES (?, ?, ?)", date, price, type)
+
+        return redirect("/transactions")
+
+    return render_template("transactions.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
